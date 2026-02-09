4e6bac2aa8d8adb2f97c7ed5218d1f1dec32184f
60292024311i-hue <6029_2024_311i@g.miyazaki-c.ed.jp>
2026-02-09 13:41:35 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ function login() {
 
   if (input === PASSWORD) {
     sessionStorage.setItem("auth", "true");
-    location.href = "secret.html";
+    location.href = "https://sites.google.com/d/1nzQdI27HrVgrf4BI8b2yt4wnoKGwL0-a/p/1TfH-epYU0gTkYQRbqLjIXEG2buSmcvNZ/edit";
   } else {
     document.getElementById("error").textContent = "パスワードが違います";
   }
